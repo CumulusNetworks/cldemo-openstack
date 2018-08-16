1a163d612dd4535f9d6460c8840f32e1402e2554
Jason Guy <jguy@cumulusnetworks.com>
2018-08-16 14:47:37 -0400

    updated openstack common to do some of the ssim cleanup

--- setup-ansible.sh
+++ setup-ansible.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+#Fix the ssim hosts
+route del default
+route add default gw 192.168.0.2 eth0
+rm /etc/apt/sources.list.d/elasticsearch-2.x.list
+echo 'Acquire::ForceIPv4 "true";' > /etc/apt/apt.conf.d/99force-ipv4
+
 # Usage Example:
 # ./setup-ansible.sh [version]
 # Look up https://github.com/ansible/ansible for version tags to use
@@ -10,19 +16,16 @@ versionTag=$1
 # Install git and other utils
 echo "[Install Ansible] Install git and sshpass"
 apt update
-apt install -y python3 sshpass git
+apt install -y python3 sshpass git python-setuptools
 
 #Install pip
 echo "[Install Ansible] Install git and sshpass"
-easy_install pip3
+easy_install pip
 
 # Install Ansible
 echo '[Install Ansible] Install Ansible'
-pip3 install ansible
+pip install ansible
 
 echo '[Install Ansible] Done!'
 
-#Fix the ssim hosts
-route del default
-route add default gw 192.168.0.2 eth0
-rm /etc/apt/sources.list.d/elasticsearch-2.x.list
+
