06303cd65658d078967af002a6c56f69033b27ee
Jason Guy <jguy@cumulusnetworks.com>
2018-08-16 14:25:57 -0400

    updated install script

--- setup-ansible.sh
+++ setup-ansible.sh
@@ -4,20 +4,25 @@
 # ./setup-ansible.sh [version]
 # Look up https://github.com/ansible/ansible for version tags to use
 
+#not used yet
 versionTag=$1
 
-# Install pip
-echo "[Install Ansible] Install pip"
+# Install git and other utils
+echo "[Install Ansible] Install git and sshpass"
 apt update
-apt install -y python-pip sshpass git
-pip install -U pip
+apt install -y python3 sshpass git
+
+#Install pip
+echo "[Install Ansible] Install git and sshpass"
+easy_install pip3
 
 # Install Ansible
 echo '[Install Ansible] Install Ansible'
-pip install ansible
+pip3 install ansible
 
 echo '[Install Ansible] Done!'
 
+#Fix the ssim hosts
 route del default
 route add default gw 192.168.0.2 eth0
 rm /etc/apt/sources.list.d/elasticsearch-2.x.list
